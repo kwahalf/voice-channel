c55c411c92fd003345508ef9eeec72284f3f2b24
kwahalf <kwanusud@gmail.com>
2021-02-04 12:06:46 +0300

    Add flow and menus

--- api/blueprints/__init__.py
+++ api/blueprints/__init__.py
@@ -16,9 +16,43 @@ VOICE_CALLBACK__REJECT_TEMPLATE = """<?xml version="1.0" encoding="UTF-8"?>
 
 GET_DIGIT_TEMPLATE = """<?xml version="1.0" encoding="UTF-8"?>
 <Response>
-    <GetDigits timeout="30" finishOnKey="#" callbackUrl="https://voice-ivr.herokuapp.com/hooks/get_digit_webhook">
-        <Say>Please enter your account number followed by the hash sign</Say>
+    <GetDigits timeout="15" finishOnKey="#" callbackUrl="https://voice-ivr.herokuapp.com/hooks/get_digit_webhook/account/1">
+        <Say>Welcome to ploar solar Self service portal, please enter your account number followed by the hash sign to proceed</Say>
+    </GetDigits>
+    <GetDigits timeout="15" finishOnKey="#" callbackUrl="https://voice-ivr.herokuapp.com/hooks/get_digit_webhook/account/1">
+        <Say> please enter your account number followed by the hash sign to proceed</Say>
     </GetDigits>
     <Say>We did not get your account number. Good bye</Say>
+</Response>
+"""
+
+GET_OPTION_TEMPLATE = """<?xml version="1.0" encoding="UTF-8"?>
+<Response>
+    <GetDigits timeout="15" finishOnKey="#" callbackUrl="https://voice-ivr.herokuapp.com/hooks/get_digit_webhook/option/{account}">
+        <Say>To check your loan balance press 1 followed by hash sign  to check your most recent token press 2 followed by hash sign</Say>
+    </GetDigits>
+    <GetDigits timeout="15" finishOnKey="#" callbackUrl="https://voice-ivr.herokuapp.com/hooks/get_digit_webhook/option/{account}">
+        <Say> To check your loan balance press 1 followed by hash sign  to check your most recent token press 2 followed by hash sign</Say>
+    </GetDigits>
+    <Say>We did not get your option. Good bye</Say>
+</Response>
+"""
+
+GET_BALANCE_TEMPLATE = """<?xml version="1.0" encoding="UTF-8"?>
+<Response>
+    <Say>Your loan balance is {balance} kenyan shilings. I repeat your loan balance is {balance} kenyan shilings.</Say>
+</Response>
+"""
+
+
+GET_TOKEN_TEMPLATE = """<?xml version="1.0" encoding="UTF-8"?>
+<Response>
+    <Say>Your latest keycode token is {token}. I repeat your latest keycode token is {token}</Say>
+</Response>
+"""
+
+WRONG_OPTION_TEMPLATE = """<?xml version="1.0" encoding="UTF-8"?>
+<Response>
+    <Say>The option you selected does not exist. Good bye</Say>
 </Response>
 """
--- api/blueprints/voice_callback.py
+++ api/blueprints/voice_callback.py
@@ -4,14 +4,23 @@ from flask.views import MethodView
 from api.util import jsonify, dejsonify, sanitize_arguments
 import logging
 
-from api.blueprints import VOICE_CALLBACK__REJECT_TEMPLATE, GET_DIGIT_TEMPLATE, make_template_file
+from api.blueprints import (
+    VOICE_CALLBACK__REJECT_TEMPLATE,
+    GET_DIGIT_TEMPLATE,
+    GET_OPTION_TEMPLATE,
+    GET_BALANCE_TEMPLATE,
+    GET_TOKEN_TEMPLATE,
+    WRONG_OPTION_TEMPLATE,
+    make_template_file
+
+    )
+
 
 logger = logging.getLogger("voice")
 logging.basicConfig(level=logging.DEBUG)
 
-class Voice(MethodView):
-    """user endpoint."""
 
+class WebhookBase(MethodView):
     def emit_success_message(self):
         message = {"status": "SUCCESS"}
         jsonified_message = jsonify(dict(message=message))
@@ -32,6 +41,29 @@ class Voice(MethodView):
             GET_DIGIT_TEMPLATE
         )
 
+    def make_get_option_payload(self, account):
+        return make_template_file(
+            GET_OPTION_TEMPLATE.format(account=account)
+        )
+
+    def make_get_balance_payload(self, balance):
+        return make_template_file(
+            GET_BALANCE_TEMPLATE.format(account=balance)
+        )
+
+    def make_get_token_payload(self, token):
+        return make_template_file(
+            GET_TOKEN_TEMPLATE.format(account=token)
+        )
+
+    def make_get_wrong_option_payload(self):
+        return make_template_file(
+            WRONG_OPTION_TEMPLATE
+        )
+
+
+
+class Voice(WebhookBase):
     def get(self):
         # Webhook verification
         return self.emit_success_message()
@@ -63,29 +95,12 @@ class Voice(MethodView):
         return self.emit_xml_response(xml)
 
 
-class GetDigit(MethodView):
-    """user endpoint."""
-
-    def emit_success_message(self):
-        message = {"status": "SUCCESS"}
-        jsonified_message = jsonify(dict(message=message))
-        response = flask.Response(jsonified_message, mimetype="application/json")
-        return response
-
-    def emit_xml_response(self, xml):
-        response = flask.Response(xml, mimetype="text/xml")
-        return response
-
-    def make_reject_payload(self):
-        return make_template_file(
-            VOICE_CALLBACK__REJECT_TEMPLATE
-        )
-
+class GetDigit(WebhookBase):
     def get(self):
         # Webhook verification
         return self.emit_success_message()
 
-    def post(self):
+    def post(self, digit_type, value):
         logger.info(
             "received notification %s",
                 flask.request.values
@@ -93,12 +108,31 @@ class GetDigit(MethodView):
 
         raw_data = flask.request.data
 
+        digits =  flask.request.form.get("dtmfDigits", None)
+
         # parsed_data = xmltodict.parse(raw_data, process_namespaces=False)
 
         # logger.info(
         #     "paseed data is %s",
         #         parsed_data
         # )
+
+        if digit_type == "account":
+            xml = self.make_get_option_payload(digits)
+            return self.emit_xml_response(xml)
+
+        if digit_type == "option":
+            if digits == "1":
+                xml = self.make_get_balance_payload(300)
+                return self.emit_xml_response(xml)
+            if digits == "2":
+                xml = self.make_get_token_payload("* 1 2 4 6 9 0 7 9 #")
+                return self.emit_xml_response(xml)
+            xml = self.make_get_wrong_option_payload()
+            return self.emit_xml_response(xml)
+
+
+
 
         xml = self.make_reject_payload()
 
@@ -112,6 +146,6 @@ def create_blueprint(at_prefix, **kwargs):
     """webhook blueprint"""
     blueprint = flask.Blueprint("webhook", __name__)
     blueprint.add_url_rule("/webhook", view_func=Voice.as_view("webhook"))
-    blueprint.add_url_rule("/get_digit_webhook", view_func=GetDigit.as_view("get_digit_webhook"))
+    blueprint.add_url_rule("/get_digit_webhook/<digit_type>/<value>", view_func=GetDigit.as_view("get_digit_webhook"))
 
     return blueprint
